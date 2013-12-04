b0423671c417ebffb1c6bab151543a6fe81d373e
Susanne Dewein <susanne.dewein@gmail.com>
2013-12-04 17:34:40 +0100

    Add methods to join and leave courses

--- studentin.rb
+++ studentin.rb
@@ -15,7 +15,8 @@ class Studentin
   end
  
   def to_s
-    "Name: " + @name + ", Vorname: " + @vorname + ", Studiengang: " + @studiengang.to_s + ", Matrikelnummer:" + @matrikelnummer.to_s + ", Kurs:" + @kurs.to_s
+    #"Name: " + @name + ", Vorname: " + @vorname + ", Studiengang: " + @studiengang.to_s + ", Matrikelnummer:" + @matrikelnummer.to_s
+    "Name: #{@name}, Vorname: #{@vorname}, Studiengang: #{@studiengang}, Matrikelnummer: #{@matrikelnummer}"
   end
  
   def wechseln!
@@ -24,8 +25,31 @@ class Studentin
  
   def drucke_wechsel
     @vorname + " " + @name + " studiert jetzt " + @neues_fach
- 
   end
+
+  def join_course(course) # NB!!!!
+    @courses ||= [] 
+    @courses << course # adds course to @courses array
+    course.enlist(self) #wischdischhh: calls on same course "enlist" 
+  end
+
+  def leave_course(course)
+    @courses.delete(course) #deletes course from array
+    course.drop_out(self) #simultaneously calls "drop_out" on course
+  end
+
+  def all_courses
+    @courses 
+  end
+
+  def check_amount_courses 
+    if @courses.count > 5 
+      puts "Enuff!" 
+    else
+      puts "need moar!"
+    end
+  end
+
 end
 
 
