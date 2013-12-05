d1f85e633c01a5288bdef70b818be5c74d536df2
Susanne Dewein <susanne.dewein@gmail.com>
2013-12-05 17:30:29 +0100

    generate some output

--- main.rb
+++ main.rb
@@ -4,24 +4,48 @@ require './course.rb'
 require './studiengang.rb'
 
 anglistik = Studiengang.new("Anglistik", "FB2")
+germanistik = Studiengang.new("Germanistik", "FB8")
 
-kurs1 = Course.new("asdf", "jaifw", "xyz")
+kurs1 = Course.new("asdf", "jaifw", germanistik)
 kurs2 = Course.new("Schönschreiben", "Schreiben wie ein junger Gott!", anglistik)
-kurs3 = Course.new("Malen nach Zahlen", "In Windeseile zum neuen Van Gogh!", "Kunst")
+kurs3 = Course.new("Malen nach Zahlen", "In Windeseile zum neuen Van Gogh!", anglistik)
 
-susi = Studentin.new("Müller", "Susanne", anglistik, 123456 )
+students = []
+
+students << susi = Studentin.new("Müller", "Susanne", anglistik, 123456 )
+students << lulu = Studentin.new("Müller", "Lulu", anglistik, 9482892 )
+students << mimi = Studentin.new("Müller", "Mimi", germanistik, 123116 )
+students << lili = Studentin.new("Müller", "Lili", anglistik, 1234564 )
+students << anna = Studentin.new("Müller", "Anna", anglistik, 1243456 )
+students << emma = Studentin.new("Müller", "Emma", anglistik, 1123456 )
 
 
-susi.join_course(kurs3)
-susi.join_course(kurs2)
-puts susi.courses
-puts "_________________"
-susi.leave_course(kurs2)
-puts susi.courses
-puts susi.check_amount_courses
+students.each do |s|
+  puts s
+end
 
-puts susi.leave_course(kurs2)
-puts kurs2.studentinnen.count
+
+# susi.join_course(kurs3)
+# susi.join_course(kurs2)
+# puts susi.courses
+# puts "_________________"
+# susi.leave_course(kurs2)
+# puts susi.courses
+# puts susi.check_amount_courses
+
+# puts susi.leave_course(kurs2)
+# puts kurs2.studentinnen.count
+kurs1.gain(susi)
+kurs1.gain(lulu)
+kurs1.gain(lili)
+kurs1.gain(mimi)
+kurs1.gain(anna)
+kurs1.gain(emma)
+
+
+puts kurs1.studentinnen.length
+
+
 
 
 
