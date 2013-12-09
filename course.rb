9996b9a142f3eb83f4a5b6f98d4af93f9c640e63
Susanne Dewein <susanne.dewein@gmail.com>
2013-12-09 14:54:04 +0100

    Add restrictions on entering and leaving a course
    a student can now only leave or enter a course if she is or is not already in the course

--- course.rb
+++ course.rb
@@ -18,22 +18,32 @@ class Course
   end
 
   def gain(studentin)
-    if @studentinnen.length < 5
+    @studentinnen.each do |s|
+      if s.vorname == studentin.vorname
+        raise Exception.new("#{studentin.vorname} #{studentin.name} nimmt bereits an #{self.title} teil.")
+      end
+    end
+    #does that work?
+
+
+    if @studentinnen.length < 2 
       @studentinnen << studentin
       puts "#{studentin.name}, #{studentin.vorname} belegt jetzt #{self.title}"
     else
       raise Exception.new("#{self.title} ist voll, tut uns leid Frau #{studentin.name}")
     end
+
   end
 
   def lose(studentin)
-    @studentinnen.delete(studentin)
-    puts "#{studentin.vorname} #{studentin.name} hat den Kurs #{self.title} verlassen."
+    @studentinnen.each do |s|
+      if not s.vorname == studentin.vorname
+        puts "You can't leave a course that you are not participating in!"
+      else
+        @studentinnen.delete(studentin)
+        puts "#{studentin.vorname} #{studentin.name} hat den Kurs #{self.title} verlassen."
+      end
+    end
   end
 
-
 end
-
-#kurs ist voll bei 5 studentin
-#studentinnen können sich nicht mehr einschreiben, wenn mehr als 5
-#methode schreiben, die überprüft ob frei
