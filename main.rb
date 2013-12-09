b07642ecdcfa7feb247b6867d2d9c0ed7430ebb2
Susanne Dewein <susanne.dewein@gmail.com>
2013-12-09 14:55:12 +0100

    Allow user input in console

--- main.rb
+++ main.rb
@@ -3,12 +3,16 @@ require './studentin.rb'
 require './course.rb'
 require './studiengang.rb'
 
-anglistik = Studiengang.new("Anglistik", "FB2")
-germanistik = Studiengang.new("Germanistik", "FB8")
+subjects = []
 
-kurs1 = Course.new("asdf", "jaifw", germanistik)
-kurs2 = Course.new("Schönschreiben", "Schreiben wie ein junger Gott!", anglistik)
-kurs3 = Course.new("Malen nach Zahlen", "In Windeseile zum neuen Van Gogh!", anglistik)
+subjects << anglistik = Studiengang.new("Anglistik", "FB2")
+subjects << germanistik = Studiengang.new("Germanistik", "FB8")
+
+courses = []
+
+courses << kurs1 = Course.new("asdf", "jaifw", germanistik)
+courses << kurs2 = Course.new("Schönschreiben", "Schreiben wie ein junger Gott!", anglistik)
+courses << kurs3 = Course.new("Malen nach Zahlen", "In Windeseile zum neuen Van Gogh!", anglistik)
 
 students = []
 
@@ -24,6 +28,7 @@ students.each do |s|
   puts s
 end
 
+
 susi.join_course(kurs1)
 lulu.join_course(kurs1)
 mimi.join_course(kurs1)
@@ -37,5 +42,53 @@ helga.leave_course(kurs1)
 puts kurs1.studentinnen.length
 
 susi.wechseln!(germanistik)
-puts susi
+
+while true
+
+  puts "What Studentin are you looking for?"
+
+  studentin = gets.chomp
+  students.each do |s|
+    if s.vorname == studentin
+      @studentin = s
+      puts "I know her!" 
+    # else
+    #   puts "I have no idea who that is." --> has to go in again!
+    # end
+    end
+  end
+
+  puts "Do you want #{studentin} to join a course (please type in A) or to leave a course (please type in B)?"
+
+  purpose = gets.chomp
+  if purpose == "A"
+    purpose = "studentin.join_course"
+  elsif purpose == "B"
+    purpose = "studentin.leave_course"
+  else
+    puts "This option is unknown to me"
+  end
+
+  puts "Please enter the title of the course."
+
+  course_title = gets.chomp
+
+  courses.each do |c|
+    @course = c if course_title == c.title
+  end
+
+  if purpose == "studentin.join_course"
+   @studentin.join_course(@course)                                                        
+  elsif purpose == "studentin.leave_course"
+    @studentin.leave_course(@course)
+  else 
+    puts "Don't know what's going on"
+  end
+
+end
+
+    
+  
+
+
 
